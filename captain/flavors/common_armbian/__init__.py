a1e5916caf0520f37157d269f6975585963dffb5
Ricardo Pardini <ricardo@pardini.net>
2026-04-04 23:37:50 +0200

    flavors: introduce flavors
    but logging sucks and no dataclass
    
    better, logging still sucks for Full -> Common
    
    much better in-package
    
    this sucks less
    
    cosmetics
    
    some static and template rendering and hardcoded cleanup
    
    kinda-works
    
    kinda works
    
    Signed-off-by: Ricardo Pardini <ricardo@pardini.net>

---
+++ captain/flavors/common_armbian/__init__.py
@@ -0,0 +1,36 @@
+import logging
+from dataclasses import dataclass
+from pathlib import Path
+
+from captain.config import Config
+from captain.flavors.common_debian import DebianCommonFlavor
+
+log: logging.Logger = logging.getLogger(__name__)
+
+
+@dataclass
+class ArmbianCommonFlavor(DebianCommonFlavor):
+    id = "common-armbian"
+    name = "Armbian Common"
+    description = "Base flavor for Armbian-based distros"
+
+    def setup(self, cfg: Config, flavor_dir: Path) -> None:
+        super().setup(cfg, flavor_dir)
+        log.warning(
+            "ArmbianCommonFlavor setting up; mkosi arch: %s; flavor_dir: %s",
+            cfg.arch_info.mkosi_arch,
+            flavor_dir,
+        )
+        this_flavor_dir = self.specific_flavor_dir("common-armbian")
+
+        # Now, lets enumerate and add all the static files this flavor's mkosi.sandbox directory
+        # and add them to self.static_map with the key being the relative path from the flavor dir
+        extra_dir = this_flavor_dir / "mkosi.sandbox"
+        if extra_dir.exists() and extra_dir.is_dir():
+            for extra_file in extra_dir.rglob("*"):
+                if extra_file.is_file():
+                    relative_path = extra_file.relative_to(this_flavor_dir)
+                    self.static_map[str(relative_path)] = extra_file
+
+    def extra_mkosi_conf_distribution(self) -> str:
+        return "# frak this"
---
+++ captain/flavors/trixie_full/__init__.py
@@ -0,0 +1,32 @@
+import logging
+from dataclasses import dataclass
+from pathlib import Path
+
+from captain.config import Config
+from captain.flavor import BaseFlavor
+from captain.flavors.common_debian import DebianCommonFlavor
+
+log: logging.Logger = logging.getLogger(__name__)
+
+
+def create_flavor() -> BaseFlavor:
+    return TrixieFullFlavor()
+
+
+@dataclass
+class TrixieFullFlavor(DebianCommonFlavor):
+    id = "trixie-full"
+    name = "Trixie Full"
+    description = "Debian Trixie based with linux-image-generic standard Debian kernel"
+    supported_architectures = frozenset(["amd64", "arm64"])
+
+    def setup(self, cfg: Config, flavor_dir: Path) -> None:
+        super().setup(cfg, flavor_dir)
+        log.warning(
+            "TrixieFullFlavor setting up; mkosi arch: %s; flavor_dir: %s",
+            cfg.arch_info.mkosi_arch,
+            flavor_dir,
+        )
+
+    def kernel_packages(self) -> set[str]:
+        return {"linux-image-generic"}
