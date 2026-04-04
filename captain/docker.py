048fc874e0a90f7f8c62e8cb88d159e9b19d183a
Ricardo Pardini <ricardo@pardini.net>
2026-04-04 23:37:50 +0200

    flavors: introduce flavors
    but logging sucks and no dataclass
    
    better, logging still sucks for Full -> Common
    
    much better in-package
    
    this sucks less
    
    cosmetics
    
    some static and template rendering and hardcoded cleanup
    
    kinda-works
    
    kinda works
    
    Signed-off-by: Ricardo Pardini <ricardo@pardini.net>

--- captain/docker.py
+++ captain/docker.py
@@ -188,6 +188,7 @@ def run_in_builder(cfg: Config, *extra_args: str) -> None:
 
     docker_args += ["-v", f"{cfg.project_dir}/mkosi.output:/work/mkosi.output"]
     docker_args += ["-v", f"{cfg.project_dir}/mkosi.extra:/work/mkosi.extra"]
+    docker_args += ["-v", f"{cfg.project_dir}/mkosi.sandbox:/work/mkosi.sandbox"]
     docker_args += ["-v", f"{cfg.project_dir}/out:/work/out"]
 
     docker_args += ["-v", f"{cfg.project_dir}/mkosi.conf:/work/mkosi.conf"]
