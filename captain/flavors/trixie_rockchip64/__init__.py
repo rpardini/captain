f893f8a7a3d9b73eb7ef32a05c65561b02e7e6f4
Ricardo Pardini <ricardo@pardini.net>
2026-04-06 12:08:11 +0200

    flavors: cleanup a bit
    Signed-off-by: Ricardo Pardini <ricardo@pardini.net>

--- captain/flavors/trixie_rockchip64/__init__.py
+++ captain/flavors/trixie_rockchip64/__init__.py
@@ -1,8 +1,6 @@
 import logging
 from dataclasses import dataclass
-from pathlib import Path
 
-from captain.config import Config
 from captain.flavor import BaseFlavor
 from captain.flavors.common_armbian import ArmbianCommonFlavor
 
@@ -19,14 +17,6 @@ class TrixieRockchip64Flavor(ArmbianCommonFlavor):
     name = "Trixie for Rockchip 64-bit ARM machines"
     description = "Debian Trixie based with Armbian's rockchip64-edge kernel"
     supported_architectures = frozenset(["arm64"])  # does NOT support amd64
-
-    def setup(self, cfg: Config, flavor_dir: Path) -> None:
-        super().setup(cfg, flavor_dir)
-        log.warning(
-            "TrixieRockchip64Flavor setting up; mkosi arch: %s; flavor_dir: %s",
-            cfg.arch_info.mkosi_arch,
-            flavor_dir,
-        )
 
     def kernel_packages(self) -> set[str]:
         return {"linux-image-edge-rockchip64"}
