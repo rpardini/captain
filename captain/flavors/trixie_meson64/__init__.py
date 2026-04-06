0e9b27dacf465b71046cda8ba1ba685530b47734
Ricardo Pardini <ricardo@pardini.net>
2026-04-06 12:10:06 +0200

    flavors/gha: introduce meson64 flavor
    Signed-off-by: Ricardo Pardini <ricardo@pardini.net>

---
+++ captain/flavors/trixie_meson64/__init__.py
@@ -0,0 +1,22 @@
+import logging
+from dataclasses import dataclass
+
+from captain.flavor import BaseFlavor
+from captain.flavors.common_armbian import ArmbianCommonFlavor
+
+log: logging.Logger = logging.getLogger(__name__)
+
+
+def create_flavor() -> BaseFlavor:
+    return TrixieMeson64Flavor()
+
+
+@dataclass
+class TrixieMeson64Flavor(ArmbianCommonFlavor):
+    id = "trixie-meson64"
+    name = "Trixie for Meson (Amlogic) 64-bit ARM machines"
+    description = "Debian Trixie based with Armbian's meson64-edge kernel"
+    supported_architectures = frozenset(["arm64"])  # does NOT support amd64
+
+    def kernel_packages(self) -> set[str]:
+        return {"linux-image-edge-meson64"}
