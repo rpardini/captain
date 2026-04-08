f1187f68c45f27690c28f7bd6bdb29869a2c015e
Ricardo Pardini <ricardo@pardini.net>
2026-04-08 11:03:33 +0200

    flavors: tighten semantics, refactor
    - refactor add_static_dir() into BaseFlavor
    - make has_iso() abstractmethod at BaseFlavor level
    - make kernel_packages() abstractmethod at common-debian level
    
    Signed-off-by: Ricardo Pardini <ricardo@pardini.net>

--- captain/flavors/common_armbian/__init__.py
+++ captain/flavors/common_armbian/__init__.py
@@ -16,13 +16,9 @@ class ArmbianCommonFlavor(DebianCommonFlavor):
 
     def setup(self, cfg: Config, flavor_dir: Path) -> None:
         super().setup(cfg, flavor_dir)
-        this_flavor_dir = self.specific_flavor_dir("common-armbian")
 
-        # Now, lets enumerate and add all the static files this flavor's mkosi.sandbox directory
-        # and add them to self.static_map with the key being the relative path from the flavor dir
-        extra_dir = this_flavor_dir / "mkosi.sandbox"
-        if extra_dir.exists() and extra_dir.is_dir():
-            for extra_file in extra_dir.rglob("*"):
-                if extra_file.is_file():
-                    relative_path = extra_file.relative_to(this_flavor_dir)
-                    self.static_map[str(relative_path)] = extra_file
+        this_flavor_dir = self.specific_flavor_dir("common-armbian")
+        self.add_static_dir("mkosi.sandbox", this_flavor_dir)
+
+    def has_iso(self) -> bool:
+        return False
