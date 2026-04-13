34c02044f407869076dccf5e3391080771c6844e
Ricardo Pardini <ricardo@pardini.net>
2026-04-14 01:12:15 +0200

    flavors: introduce `trixie-slim` flavor, using captainos kernel
    - this one uses the captain-build-from-source kernel .deb
    - let's violate some package boundaries:
      - cli acrobatics: delegates to kernel build stage before initramfs build
      - impls package_directories() by calling kernel
    
    Signed-off-by: Ricardo Pardini <ricardo@pardini.net>

---
+++ captain/flavors/trixie_slim/__init__.py
@@ -0,0 +1,32 @@
+import logging
+from dataclasses import dataclass
+
+from captain.cli import stages
+from captain.flavor import BaseFlavor
+from captain.flavors.common_acpi import TrixieACPIFlavor
+from captain.kernel import obtain_target_artifact_path
+
+log: logging.Logger = logging.getLogger(__name__)
+
+
+def create_flavor() -> BaseFlavor:
+    return TrixieSlimFlavor()
+
+
+@dataclass
+class TrixieSlimFlavor(TrixieACPIFlavor):
+    id = "trixie-slim"
+    name = "Trixie Slim"
+    description = "Debian Trixie based with captainos slim kernel"
+    supported_architectures = frozenset(["amd64", "arm64"])
+
+    def pre_mkosi_stage(self):
+        log.debug("Flavor delegating to build_kernel_stage to ensure kernel .deb is present")
+        # call the kernel build stage, to ensure kernel .deb is in mkosi.output/kernel/<arch>
+        stages.build_kernel_stage(self.cfg)
+
+    def kernel_packages(self) -> set[str]:
+        return {f"linux-image-{self.cfg.kernel_version}-captainos"}
+
+    def package_directories(self) -> set[str]:
+        return {str(obtain_target_artifact_path(self.cfg).parent.relative_to(self.cfg.project_dir))}
