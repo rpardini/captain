428d37877b6dad25560e79558743bd000e586971
Ricardo Pardini <ricardo@pardini.net>
2026-04-15 21:11:53 +0200

    cli: bring back `qemu` cli, with extra support for arm64 & UEFI/OVMF
    - arm64 qemu requires OVMF/AAVMF firmware (edk2) via a pflash
    - use `q35` machine type for amd64 (it is still not a default)
    - add kvm (Linux) and hvf (Darwin) acceleration if present and matching host/target arch
    - add `systemd.journald.forward_to_console=1` as default prefix kernel cmdline
    - remove `audit=0` as cmdline prefix default
    - run with eg `uv run captain --verbose --arch=amd64 --flavor-id=trixie-slim qemu -- tink_worker_image=... <...>`
      - extra args after `--` are passed directly as kernel cmdline
      - maybe restore option/env parsing with Click later (kept TODO'ed `_TINK_PARAMS`) for ergonomics
    - qemu: more systemd logging in default kernel cmdline
    - qemu: run x86 also under OVMF (UEFI), not BIOS emulation
      - this more accurately emulates real environment
        - real EFI implementation, with efivars etc
      - unify accel handling (accel only when host arch matches target arch)
        - but then it's the same across arches: hcf for Apple, kvm for Linux
      - refactor find_ovmf_firmware() as it's common
    
    Signed-off-by: Ricardo Pardini <ricardo@pardini.net>

---
+++ captain/cli/_qemu.py
@@ -0,0 +1,36 @@
+"""``captain tools`` — download tools (containerd, runc, nerdctl, CNI plugins)."""
+
+from __future__ import annotations
+
+import logging
+
+import click
+from click import Context
+
+from captain.cli._main import CliContext, cli
+from captain.qemu import run_qemu
+
+log = logging.getLogger(__name__)
+
+
+@cli.command(
+    "qemu",
+    context_settings={
+        "ignore_unknown_options": True,
+        "allow_extra_args": True,
+    },
+    short_help="Run the built captainos kernel+initramfs in QEMU.",
+)
+@click.pass_obj
+@click.pass_context
+def shell_cmd(ctx: Context, cli_ctx: CliContext) -> None:
+    log.debug("Running captainos in QEMU...")
+
+    cfg = cli_ctx.make_config()
+
+    extra_args: list[str] = ctx.args
+    log.debug(f"Extra args passed to qemu: {extra_args}")
+
+    run_qemu(cfg, extra_args)
+
+    log.info("qemu exited.")
