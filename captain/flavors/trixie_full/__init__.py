a5ba1a26f912f8726820e7b1bb113966300e5593
Ricardo Pardini <ricardo@pardini.net>
2026-04-16 17:34:00 +0200

    flavors: flavor_packages() hierarchy, include_apt(), include_hwdb()
    - `include_apt()`: if true, includes a working apt install
    - `include_hwdb()`: if true, leaves hwdb data in
    - move `acpid` package to common_acpi flavor
    - also always include `curl`, `pciutils`, `pv`
    
    Signed-off-by: Ricardo Pardini <ricardo@pardini.net>

--- captain/flavors/trixie_full/__init__.py
+++ captain/flavors/trixie_full/__init__.py
@@ -18,5 +18,17 @@ class TrixieFullFlavor(TrixieACPIFlavor):
     description = "Debian Trixie based with linux-image-generic standard Debian kernel"
     supported_architectures = frozenset(["amd64", "arm64"])
 
-    def kernel_packages(self) -> set[str]:
-        return {"linux-image-generic"}
+    def flavor_packages(self) -> set[str]:
+        pkgs = {
+            "linux-image-generic",  # Debian's standard kernel (arm64/amd64)
+            "tiny-initramfs",  # A tiny initramfs builder; required for kernel image
+        }
+        if self.include_working_apt():
+            pkgs.add("apt")
+        return pkgs.union(super().flavor_packages())
+
+    def include_hwdb(self) -> bool:
+        return True
+
+    def include_working_apt(self) -> bool:
+        return True
